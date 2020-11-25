d251cc8759f93e002d38717ab82baad30160138f
denisbut1212 <denisbut1212@gmail.com>
2020-11-25 07:03:35 +0300

    tests for git

---
+++ Assets/Scripts/Git.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using UnityEngine;
+
+public static class Git
+{
+    public static string GetLastCommit()
+    {
+        var pathToGitFolder = Path.Combine(Application.dataPath.Remove(Application.dataPath.Length - 7), ".git");
+        var currentRef = 
+            File.ReadAllText(Path.Combine(pathToGitFolder, "HEAD")).Remove(0, 16).TrimEnd();
+        return File.ReadAllText(Path.Combine(pathToGitFolder, "refs", "heads", currentRef)).TrimEnd();
+    }
+}
---
+++ Assets/Tests/Git.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class GitTests
+    {
+        [Test]
+        public void GetLastCommitTest()
+        {
+            const string expected = "b672ccf7ab13a4469dd0b477f4f974800da1d661";
+            Assert.AreEqual(expected, Git.GetLastCommit());
+        }
+    }
+}
